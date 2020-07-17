15a2d61b63bbc90deefe920e0e60b7d360251e58
Guzuf Luna <36116338+The10axe@users.noreply.github.com>
2020-07-17 19:23:36 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,89 @@
+import discord
+import datetime
+
+client = discord.Client()
+prefix = "/"
+
+@client.event
+async def on_ready():
+    print('Logged in as '+ str(client.user))
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	
+	if message.content.startswith(prefix+'help'):
+		print("/help done by "+str(message.author)+"("+str(message.author.id)+") at "+str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+		info = await client.application_info()
+		async with message.channel.typing():
+			if info.owner == message.author:
+				embed = discord.Embed(title="Help Page", description="For Owner and for Dummies", color=message.author.color)
+			else:
+				embed = discord.Embed(title="Help Page", description="For Users and for Dummies", color=message.author.color)
+			embed.add_field(name="/help", value="This is what you just did!")
+			embed.add_field(name="/time", value="Gives you the Bot's time.")
+			embed.add_field(name="/info [user]", value="Gives you info about a user, if no user is given, gives info about you")
+			if info.owner == message.author:
+				embed.add_field(name="/stop", value="Stop the bot!")
+			embed.set_footer(text=str(message.author), icon_url=message.author.avatar_url)
+			await message.channel.send(content=None,tts=False,embed=embed)
+		return
+	
+	if message.content.startswith(prefix+'time'):
+		print("/time done by "+str(message.author)+"("+str(message.author.id)+") at "+str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+		async with message.channel.typing():
+			await message.channel.send("Il est: "+str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+		return
+
+	if message.content.startswith(prefix+'info'):
+		print("/info done by "+str(message.author)+"("+str(message.author.id)+") at "+str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+		async with message.channel.typing():
+			if len(message.mentions) == 0:
+				embed = discord.Embed(title="Info about "+str(message.author), description=None, color=message.author.color)
+				embed.set_thumbnail(url=message.author.avatar_url)
+				embed.add_field(name="ID:", value=str(message.author.id), inline=False)
+				embed.add_field(name="Came in Discord:", value=str(message.author.created_at)[:-3], inline=False)
+				# embed.add_field(name="Activities:", value=str(message.author.activities))
+				if message.guild != None:
+					embed.add_field(name="Top role in this server:",value=str(message.author.top_role), inline=False)
+					embed.add_field(name="Nickname:", value=str(message.author.display_name), inline=False)
+					embed.add_field(name="Status:", value=str(message.author.status).capitalize(), inline=False)
+					embed.add_field(name="Came in Guild:", value=str(message.author.joined_at)[:-3], inline=False)
+			else:
+				if message.mentions[0] == client.user:
+					embed = discord.Embed(title="Info about "+str(message.mentions[0]), description=None, color=message.mentions[0].color, url="https://discord.com/api/oauth2/authorize?client_id=426478004298842113&permissions=8&redirect_uri=http%3A%2F%2Fdiscord.com%2F&scope=bot")
+				else:
+					embed = discord.Embed(title="Info about "+str(message.mentions[0]), description=None, color=message.mentions[0].color)
+				embed.set_thumbnail(url=message.mentions[0].avatar_url)
+				embed.add_field(name="ID:", value=str(message.mentions[0].id), inline=False)
+				embed.add_field(name="Came in Discord:", value=str(message.mentions[0].created_at)[:-3], inline=False)
+				# embed.add_field(name="Activities:", value=str(message.mentions[0].activities))
+				if message.guild != None:
+					embed.add_field(name="Top role in this server:",value=str(message.mentions[0].top_role), inline=False)
+					embed.add_field(name="Nickname:", value=str(message.mentions[0].display_name), inline=False)
+					embed.add_field(name="Status:", value=str(message.mentions[0].status).capitalize(), inline=False)
+					embed.add_field(name="Came in Guild:", value=str(message.mentions[0].joined_at)[:-3], inline=False)
+		embed.set_footer(text=str(message.author), icon_url=message.author.avatar_url)
+		await message.channel.send(content=None,tts=False,embed=embed)
+		return
+
+	if message.content.startswith(prefix+'stop'):
+		print("/stop done by "+str(message.author)+"("+str(message.author.id)+") at "+str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+		info = await client.application_info()
+		if info.owner == message.author:
+			async with message.channel.typing():
+				await message.channel.send("Good bye!")
+			print("Waiting for log out!")
+			await client.logout()
+			print("Shutting down...")
+			exit()
+		else:
+			return
+
+file = open("./settings/token.id", "r")
+Token = file.readline()
+file.close()
+# print("Token: "+str(Token))
+print("Discord Version is: "+str(discord.version_info.releaselevel)+" "+str(discord.__version__))
+client.run(Token)
