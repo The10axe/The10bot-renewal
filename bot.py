ed04d354064d8cfca8ff3450d3ae225b0c371138
Guzuf Luna <36116338+The10axe@users.noreply.github.com>
2020-07-25 16:30:32 +0200

    Add files via upload
    - Fixed an issue in "/help" -> /crypto
    - Fixed an issue in /crypto

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ async def on_message(message):
 			embed.add_field(name="/ping", value="Gives the latency of the bot to Discord")
 			embed.add_field(name="/bot", value="Gives information about the bot!")
 			embed.add_field(name="/seek <ID>", value="Gives you info about a user, using their ID")
-			embed.add_field(name="/crypto [-lang:<morse|binary|hexadecimal|octal] [-time:<seconds>] [-force:<true|false>] [sentences]", value="Starts a game where you need to guess what's written in a language, if nothing is given, a convertion table will be displayed, if only a sentences is given, the game start with default rules which is 300s (5 minutes) and a random language.", inline=False)
+			embed.add_field(name="/crypto [-lang:<morse|binary|hexadecimal|octal>] [-time:<seconds>] [-force:<true|false>] [sentences]", value="Starts a game where you need to guess what's written in a language, if nothing is given, a convertion table will be displayed, if only a sentences is given, the game start with default rules which is 300s (5 minutes) and a random language.", inline=False)
 			if info.owner == message.author:
 				embed.add_field(name="/stop", value="Stop the bot!")
 			embed.set_footer(text=str(message.author), icon_url=message.author.avatar_url)
@@ -209,6 +209,7 @@ async def on_message(message):
 		return
 	
 	if message.content.startswith(prefix+'crypto'):
+		print("/crypto done by "+str(message.author)+"("+str(message.author.id)+") at "+str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
 		morse = ["·-","-···","-·-·", "-··","·","··-·","--·","····","··","·---","-·-","·-··","--","-·","---","·--·","--·-","·-·","···","-","··-","···-","·--","-··-","-·--","--··","·----","··---","···--","····-","·····","-····","--···","---··","----·","-----","   "]
 		lettre = ["A", "B", "C", "D", "E", "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"," "]
 		binary = ["01000001","01000010","01000011","01000100","01000101","01000110","01000111","01001000","01001001","01001010","01001011","01001100","01001101","01001110","01001111","01010000","01010001","01010010","01010011","01010100","01010101","01010110","01010111","01011000","01011001","01011010","00110001","00110010","00110011","00110100","00110101","00110110","00110111","00111000","00111001","00110000","00100000"]
